4dda062491f7227651ea372ffbf80276995e31a0
Rachel Spencer <rachelspencer688@gmail.com>
2024-01-15 12:43:56 -0500

    complete refactor solution

--- sumOfPos2.js
+++ sumOfPos2.js
@@ -6,3 +6,22 @@
 // else return sum of all integars < 0
 
 // USING FILTER METHOD
+
+const sumOfPos = (arr) => {
+    let arrOfPos = arr.filter((num) => num > 0);
+    let sumOfPos = [];
+    
+    if (arrOfPos.length){
+        sumOfPos = arrOfPos.reduce((accumulator, currentValue) => accumulator + currentValue);
+    } else {
+        return 0;
+    }
+
+    return sumOfPos;
+};
+
+console.log("ans 15", sumOfPos([1,2,3,4,5])); //15
+console.log("ans 13", sumOfPos([1,-2,3,4,5])); //13
+console.log("ans 0", sumOfPos([])); //0
+console.log("ans 0", sumOfPos([-1,-2,-3,-4,-5])); //0
+console.log("ans 9", sumOfPos([-1,2,3,4,-5])); //9
