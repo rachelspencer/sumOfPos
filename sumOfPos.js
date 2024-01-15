9527a415c8393ad2d466c05ee7f3e62c54cfeb60
Rachel Spencer <rachelspencer688@gmail.com>
2024-01-15 11:29:39 -0500

    solution using forEach

---
+++ sumOfPos.js
@@ -0,0 +1,29 @@
+// you get an array of numbers, return the sum of all the positive ones
+// if there is nothing to sum, the sum default is 0
+
+// if array is empty return 0
+// if all integars in an array are negative return 0
+// else return sum of all integars < 0
+
+function positiveSum(arr) {
+    const positiveArr = []
+    let sum = 0;
+
+    arr.forEach((num) => {
+        if (num > 0){
+            positiveArr.push(num) 
+        }
+    });
+
+    positiveArr.forEach(num => {
+        sum += num;
+    });
+
+    return sum;
+};
+
+console.log("ans 15", positiveSum([1,2,3,4,5])); //15
+console.log("ans 13", positiveSum([1,-2,3,4,5])); //13
+console.log("ans 0", positiveSum([])); //0
+console.log("ans 0", positiveSum([-1,-2,-3,-4,-5])); //0
+console.log("ans 9", positiveSum([-1,2,3,4,-5])); //9
