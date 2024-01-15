5c32fcdd59bb77573e7a5e00c52e4427896d6604
Rachel Spencer <rachelspencer688@gmail.com>
2024-01-15 11:59:43 -0500

    create new file to refactor solution

---
+++ sumOfPos2.js
@@ -0,0 +1,8 @@
+// you get an array of numbers, return the sum of all the positive ones
+// if there is nothing to sum, the sum default is 0
+
+// if array is empty return 0
+// if all integars in an array are negative return 0
+// else return sum of all integars < 0
+
+// USING FILTER METHOD
